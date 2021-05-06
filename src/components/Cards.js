7e0b8e3437761a5d7a9b501907562550b24d65a2
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-05-06 16:41:21 +0100

    refactor code

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,32 @@
 import { useEffect, useState } from "react"
-import DisplayCards from "./DisplayCards"
+import Gameplay from "./Gameplay"
 
-function Cards() {
+const Cards = () => {
     const [char, setChar] = useState([])
     const [loading, setLoading] = useState(true)
-
 
     useEffect(() => {
         const getRick = async () => {
             try {
-                const response = await fetch("https://rickandmortyapi.com/api/character/[1, 2, 3]")
+                const characters = [1, 2, 3, 4]
+                const response = await fetch(`https://rickandmortyapi.com/api/character/${characters}`)
                 const data = await response.json()
 
-                // console.log(data)
-                setChar({ data })
+                // randomize(data)
+                setChar(data)
                 setLoading(false)
                 return data
             } catch {
                 console.error("Error")
             }
         }
+
+        // const randomize = (characters) => {
+        //     const randomizedChars = characters.sort(() => Math.random() - 0.5)
+        //     setChar(randomizedChars)
+        //     setLoading(false)
+        // }
+
         getRick()
     }, [])
 
@@ -31,7 +38,7 @@ function Cards() {
     return (
         <div>
             <button onClick={check}>Check</button>
-            {loading ? <div>Loading...</div> : <DisplayCards cards={char} />}
+            {loading ? <div>Loading...</div> : <Gameplay cards={char} />}
         </div>
     )
 }
