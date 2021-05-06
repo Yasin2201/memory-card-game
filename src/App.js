521b725a5e83e40a051080ef897926172f316e5c
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-05-06 16:40:32 +0100

    added title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Cards from "./components/Cards"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
+      <h1 id="title">Memory Card Game</h1>
       <Cards />
     </div>
   );
